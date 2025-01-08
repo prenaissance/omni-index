44b4d102140018aac5f959e5bd655c3fafa8e735
prenaissance <alex.andries.aa@gmail.com>
2025-01-08 23:17:02 +0200

    feat(synchronization): add certificat refresh endpoint

--- apps/backend/src/routes/peer-nodes/actions.ts
+++ apps/backend/src/routes/peer-nodes/actions.ts
@@ -2,7 +2,9 @@ import {
   FastifyPluginAsyncTypebox,
   Type,
 } from "@fastify/type-provider-typebox";
+import { ObjectId } from "mongodb";
 import { ExceptionSchema } from "~/common/payloads/exception-schema";
+import { ObjectIdSchema } from "~/common/payloads/object-id-schema";
 import { PeerNode } from "~/synchronization/entities/peer-node";
 import { PinnedCertificate } from "~/synchronization/entities/pinned-certificate";
 import { CreatePeerNodeRequest } from "~/synchronization/payloads/peer-node/create-peer-node-request";
@@ -18,6 +20,10 @@ const peerNodeRoutes: FastifyPluginAsyncTypebox = async (app) => {
     "",
     {
       schema: {
+        tags: ["Peer Nodes"],
+        summary: "Creates a new peer node",
+        description:
+          "Creates a new peer node with the specified hostname and trust level",
         body: Type.Ref(CreatePeerNodeRequest),
         response: {
           201: Type.Ref(PeerNodeResponse),
@@ -59,6 +65,40 @@ const peerNodeRoutes: FastifyPluginAsyncTypebox = async (app) => {
 
       await app.peerNodes.service.add(peerNode);
       return peerNode;
+    }
+  );
+
+  app.post(
+    "/:id/refresh",
+    {
+      schema: {
+        tags: ["Peer Nodes"],
+        summary: "Refreshes the pinned certificate for a peer node",
+        params: Type.Object({
+          id: ObjectIdSchema({
+            description: "ObjectId of the peer node",
+          }),
+        }),
+        response: {
+          200: Type.Ref(PeerNodeResponse),
+          404: Type.Ref(ExceptionSchema),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      const node = await app.peerNodes.repository.findOne({
+        _id: new ObjectId(id),
+      });
+      if (!node) {
+        reply.status(404);
+        return {
+          message: "Peer node not found",
+        };
+      }
+
+      await app.peerNodes.service.refresh(node);
+      return node;
     }
   );
 };
--- apps/backend/src/synchronization/services/peer-node-service.ts
+++ apps/backend/src/synchronization/services/peer-node-service.ts
@@ -1,18 +1,30 @@
 import { ObjectId } from "mongodb";
 import { PeerNode, PeerNodeInit } from "../entities/peer-node";
 import { PeerNodeRepository } from "../repositories/peer-node-repository";
+import { getCertificate } from "../utilities";
+import { PinnedCertificate } from "../entities/pinned-certificate";
 
 export class PeerNodeService {
   private readonly hostnameSet = new Set<string>();
+  private readonly fingerprintSet = new Set<string>();
   /** Cached hostnames available synchronously */
   get hostnames(): ReadonlySet<string> {
     return this.hostnameSet;
+  }
+  /** Cached fingerprints available synchronously */
+  get fingerprints(): ReadonlySet<string> {
+    return this.fingerprintSet;
   }
   constructor(private readonly peerNodeRepository: PeerNodeRepository) {}
 
   async init() {
     const peerNodes = await this.peerNodeRepository.getAll();
-    peerNodes.forEach((node) => this.hostnameSet.add(node.hostname));
+    peerNodes.forEach((node) => {
+      this.hostnameSet.add(node.hostname);
+      node.pinnedCertificates.forEach((certificate) => {
+        this.fingerprintSet.add(certificate.sha256);
+      });
+    });
   }
 
   async exists(hostname: string) {
@@ -31,6 +43,26 @@ export class PeerNodeService {
     const peerNode = new PeerNode(node);
     await this.peerNodeRepository.save(peerNode);
     this.hostnameSet.add(peerNode.hostname);
+    peerNode.pinnedCertificates.forEach((certificate) => {
+      this.fingerprintSet.add(certificate.sha256);
+    });
+  }
+
+  /** Refreshes the pinned certificate for a peer node. Removes any older certificates. */
+  async refresh(node: PeerNode) {
+    const certificate = await getCertificate(node.hostname);
+    const pinnedCertificates = node.pinnedCertificates;
+    node.pinnedCertificates = [
+      new PinnedCertificate({
+        sha256: certificate.fingerprint256,
+      }),
+    ];
+    await this.peerNodeRepository.save(node);
+
+    pinnedCertificates.forEach((certificate) => {
+      this.fingerprintSet.delete(certificate.sha256);
+    });
+    this.fingerprintSet.add(certificate.fingerprint256);
   }
 
   async deleteById(id: ObjectId) {
