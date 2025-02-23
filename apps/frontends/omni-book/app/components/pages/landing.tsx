1be057f499c5b9ae7e257d65430fde976db861ea
alya1007 <alexandra.konjevic@isa.utm.md>
2025-02-23 17:51:20 +0200

    chore: add fonts and images

---
+++ apps/frontends/omni-book/app/components/pages/landing.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+type LandingProps = {};
+
+const Landing: FC<LandingProps> = ({}) => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
