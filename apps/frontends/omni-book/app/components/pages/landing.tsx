a6d9cd1f705bedec3e8112b8dbfdb9c52306278a
alya1007 <alexandra.konjevic@isa.utm.md>
2025-02-25 22:30:51 +0200

    app.css

--- apps/frontends/omni-book/app/components/pages/landing.tsx
+++ apps/frontends/omni-book/app/components/pages/landing.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
-
+import Navbar from "../navbar";
+import "../../app.css";
 type LandingProps = {};
 
 const Landing: FC<LandingProps> = ({}) => {
   return (
     <div>
+      <Navbar />
       <h1>Landing Page</h1>
     </div>
   );
