c9873a31e18f029684ab2429a3878995dbdfa62b
prenaissance <alex.andries.aa@gmail.com>
2025-03-02 22:05:01 +0200

    feat(backend): add new env variables w/ validation

--- apps/backend/src/common/config/env.ts
+++ apps/backend/src/common/config/env.ts
@@ -13,13 +13,30 @@ export const envSchema = z.object({
   API_URL: z.string(),
   FRONTEND_URL: z.string().url(),
   CALLBACK_URL: z.string().url(),
-  INIT_SESSION_SECRET: z
-    .string()
-    .min(32, "The secret key must be at least 32 characters long")
-    .optional(),
   DANGEROUS_SKIP_IDENTITY_VERIFICATION: z
     .string()
     .transform((v) => v === "true"),
+  INIT_SESSION_SECRET: z
+    .string()
+    .min(32, "The secret key must be at least 32 characters long")
+    .or(z.string().default(""))
+    .transform((v) => v || undefined)
+    .optional(),
+  INIT_ADMIN_IDENTITY: z
+    .string()
+    // TLD lenient regex
+    .regex(
+      /[a-zA-Z0-9-]{1,63}(\.[a-zA-Z0-9-]{1,63}){1,}/,
+      "The identity must be a valid handle or a DID"
+    )
+    .or(
+      z
+        .string()
+        .regex(
+          /^did:plc:.{24}$/,
+          "The identity must be a valid handle or a DID"
+        )
+    ),
 });
 
 export type Env = z.infer<typeof envSchema>;
