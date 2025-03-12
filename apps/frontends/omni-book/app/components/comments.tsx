f5a6a6fa7850506f62536f3a94fab8f4cdeec41a
alya1007 <alexandra.konjevic@isa.utm.md>
2025-03-12 19:51:00 +0200

    feat(frontend): create comments and recommended sections

---
+++ apps/frontends/omni-book/app/components/comments.tsx
@@ -0,0 +1,11 @@
+const Comments = ({}) => {
+  return (
+    <div className="w-2/3">
+      <div className="bg-card p-4 rounded-xl">
+        <h2>Comments</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
