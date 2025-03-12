570883f755aa8d4824a52c3f58fa7a37b54fd387
alya1007 <alexandra.konjevic@isa.utm.md>
2025-03-12 19:50:38 +0200

    feat(frontend): create components for icons

--- apps/frontends/omni-book/app/components/navbar.tsx
+++ apps/frontends/omni-book/app/components/navbar.tsx
@@ -1,4 +1,7 @@
+import Menu from "./icons/menu";
+import Search from "./icons/search";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 const Navbar = ({}) => {
   return (
@@ -6,17 +9,7 @@ const Navbar = ({}) => {
       <div className="py-5 px-10 flex flex-row items-center justify-between">
         <div className="flex flex-row space-x-14">
           <Button variant={"icon"} size={"icon"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <Menu />
           </Button>
           <a className="flex flex-row space-x-2 items-center h-fit" href="/">
             <img
@@ -41,29 +34,14 @@ const Navbar = ({}) => {
                 Search
               </label>
               <div className="relative h-full">
-                <input
-                  id="landing-search"
-                  className="pl-6 pr-12 block h-full w-80 text-sm border rounded-md bg-card border-border text-card-foreground focus:ring-0 focus:border-none outline-none"
-                  placeholder="Search..."
-                />
+                <Input id="landing-search" placeholder="Search..."></Input>
                 <Button
                   type="submit"
                   variant={"icon"}
                   size={"icon"}
                   className="absolute top-0 right-1"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <Search />
                 </Button>
               </div>
             </form>
