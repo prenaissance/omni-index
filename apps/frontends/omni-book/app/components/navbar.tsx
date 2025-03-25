5b71f6af7bcfa9cb5145a0cf5617a482a66623e0
alya1007 <alexandra.konjevic@isa.utm.md>
2025-03-25 23:54:06 +0200

    feat(frontend): add profile for logged in user

--- apps/frontends/omni-book/app/components/navbar.tsx
+++ apps/frontends/omni-book/app/components/navbar.tsx
@@ -1,82 +1,173 @@
-import { Form, Link } from "react-router";
+import { Form, Outlet } from "react-router";
+import type { Route } from "../components/+types/navbar";
 import { Button } from "./ui/button";
+import type { paths } from "~/lib/api-types";
+import { env } from "~/lib/env";
 
-const Navbar = ({}) => {
+type ProfileType =
+  paths["/api/profile"]["get"]["responses"]["200"]["content"]["application/json"];
+
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const res = await fetch(`${env.VITE_API_URL}/api/profile`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      cookie: request.headers.get("cookie") ?? "",
+    },
+  });
+
+  if (!res.ok) {
+    return { user: null };
+  }
+
+  const user = (await res.json()) as ProfileType;
+  return { user };
+};
+
+const Navbar = ({ loaderData }: Route.ComponentProps) => {
+  console.log(loaderData);
   return (
-    <nav className="sticky top-0 bg-background">
-      <div className="py-5 px-10 flex flex-row items-center justify-between">
-        <div className="flex flex-row space-x-14">
-          <Button variant={"icon"} size={"icon"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Button>
-          <a className="flex flex-row space-x-2 items-center h-fit" href="/">
-            <img
-              src={"/ui-logo.svg"}
-              alt="ui-logo"
-              height={"40px"}
-              width={"40px"}
-            />
-            <div className="flex flex-col space-y-0">
-              <h6 className="leading-4 text-md">Omni</h6>
-              <h1 className="text-2xl font-bold leading-5">
-                <strong>Book</strong>
-              </h1>
-            </div>
-          </a>
-          <div className="h-10">
-            <form className="h-full">
-              <label
-                htmlFor="landing-search"
-                className="mb-2 text-sm font-medium text-foreground sr-only"
+    <>
+      <nav className="sticky top-0 bg-background">
+        <div className="py-5 px-10 flex flex-row items-center justify-between">
+          <div className="flex flex-row space-x-14">
+            <Button variant={"icon"} size={"icon"}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
               >
-                Search
-              </label>
-              <div className="relative h-full">
-                <input
-                  id="landing-search"
-                  className="pl-6 pr-12 block h-full w-80 text-sm border rounded-md bg-card border-border text-card-foreground focus:ring-0 focus:border-none outline-none"
-                  placeholder="Search..."
+                <path
+                  fillRule="evenodd"
+                  d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+                  clipRule="evenodd"
                 />
-                <Button
-                  type="submit"
-                  variant={"icon"}
-                  size={"icon"}
-                  className="absolute top-0 right-1"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </Button>
+              </svg>
+            </Button>
+            <a className="flex flex-row space-x-2 items-center h-fit" href="/">
+              <img
+                src={"/ui-logo.svg"}
+                alt="ui-logo"
+                height={"40px"}
+                width={"40px"}
+              />
+              <div className="flex flex-col space-y-0">
+                <h6 className="leading-4 text-md">Omni</h6>
+                <h1 className="text-2xl font-bold leading-5">
+                  <strong>Book</strong>
+                </h1>
               </div>
-            </form>
+            </a>
+            <div className="h-10">
+              <form className="h-full">
+                <label
+                  htmlFor="landing-search"
+                  className="mb-2 text-sm font-medium text-foreground sr-only"
+                >
+                  Search
+                </label>
+                <div className="relative h-full">
+                  <input
+                    id="landing-search"
+                    className="pl-6 pr-12 block h-full w-80 text-sm border rounded-md bg-card border-border text-card-foreground focus:ring-0 focus:border-none outline-none"
+                    placeholder="Search..."
+                  />
+                  <Button
+                    type="submit"
+                    variant={"icon"}
+                    size={"icon"}
+                    className="absolute top-0 right-1"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </Button>
+                </div>
+              </form>
+            </div>
           </div>
+          {loaderData.user ? (
+            <div className="flex flex-row space-x-4 items-center">
+              <div className="relative inline-block">
+                <input
+                  type="checkbox"
+                  id="menu-toggle"
+                  className="peer hidden"
+                />
+                <label
+                  htmlFor="menu-toggle"
+                  className="cursor-pointer flex items-center gap-4"
+                >
+                  <img
+                    src={loaderData.user.avatarUrl}
+                    alt="avatar"
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <h6 className="text-sm font-medium text-foreground">
+                      {loaderData.user.displayName}
+                    </h6>
+                  </div>
+                </label>
+                <div
+                  className="absolute right-[-20px] mt-2 w-36 rounded-md shadow-lg bg-card border border-card
+                     opacity-0 scale-95 peer-checked:opacity-100 peer-checked:scale-100 transition-all"
+                >
+                  <ul className="py-2 font-medium text-sm">
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-popover">
+                        Profile
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-popover">
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="px-4 py-2 hover:bg-popover flex items-center gap-2"
+                      >
+                        Logout
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-5 transform scale-x-[-1]"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <Form method="get" action="/login">
+                <Button>Login</Button>
+              </Form>
+            </div>
+          )}
         </div>
-        <div>
-          <Form method="get" action="/login">
-            <Button>Login</Button>
-          </Form>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <Outlet />
+    </>
   );
 };
 
--- apps/frontends/omni-book/app/routes.ts
+++ apps/frontends/omni-book/app/routes.ts
@@ -1,17 +1,20 @@
 import {
   type RouteConfig,
   index,
+  layout,
   prefix,
   route,
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("/login", "routes/login.tsx"),
-  route("/client-metadata.json", "server/routes/client-metadata.ts"),
-  ...prefix("/api", [
-    route("/oauth/callback", "server/routes/oauth/callback.ts"),
-    route("/oauth/login", "server/routes/oauth/login.ts"),
+  layout("components/navbar.tsx", [
+    index("routes/home.tsx"),
+    route("/login", "routes/login.tsx"),
+    route("/client-metadata.json", "server/routes/client-metadata.ts"),
+    ...prefix("/api", [
+      route("/oauth/callback", "server/routes/oauth/callback.ts"),
+      route("/oauth/login", "server/routes/oauth/login.ts"),
+    ]),
+    route("books/:bookId", "routes/book.tsx"),
   ]),
-  route("books/:bookId", "routes/book.tsx"),
 ] satisfies RouteConfig;
