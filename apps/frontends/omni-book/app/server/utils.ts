2450c3d1df2536de0ddd9041f77fd4c425deb09e
alya1007 <alexandra.konjevic@isa.utm.md>
2025-03-26 20:52:45 +0200

    feat(frontend): add function to parse cookie header

---
+++ apps/frontends/omni-book/app/server/utils.ts
@@ -0,0 +1,13 @@
+export const parseCookie = (cookie: string) => {
+  const parsedCookie = cookie
+    .split(";")
+    .map((v) => v.split("="))
+    .reduce(
+      (acc, v) => {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+        return acc;
+      },
+      {} as Record<string, string>
+    );
+  return parsedCookie;
+};
