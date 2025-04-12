e60036e991cbd8595407d48e2233c8d3cfc609e3
prenaissance <alex.andries.aa@gmail.com>
2025-04-13 00:42:17 +0300

    test(backend): add stored event e2e tests

--- apps/backend/src/common/payloads/pagination/pagination-query.ts
+++ apps/backend/src/common/payloads/pagination/pagination-query.ts
@@ -7,11 +7,15 @@ export const PaginationQuery = Type.Object(
         description: "Page number (1 indexed)",
         default: 1,
         minimum: 1,
-        maximum: 100,
       })
     ),
     limit: Type.Optional(
-      Type.Integer({ description: "Page size", default: 10, minimum: 10 })
+      Type.Integer({
+        description: "Page size",
+        default: 10,
+        minimum: 1,
+        maximum: 100,
+      })
     ),
   },
   {
