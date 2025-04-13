953a9b9dcc3552efa0eec3ce2b346712943b1178
alya1007 <alexandra.konjevic@isa.utm.md>
2025-04-13 19:09:28 +0300

    feat(frontend): add trust level dropdown and certificate fields

--- apps/frontends/omni-book/app/routes/admin/nodes-config.tsx
+++ apps/frontends/omni-book/app/routes/admin/nodes-config.tsx
@@ -8,6 +8,8 @@ import { Button } from "~/components/ui/button";
 import Popup from "~/components/ui/popup";
 import TrashIcon from "~/components/icons/trash";
 import ChevronIcon from "~/components/icons/chevron";
+import RegenerateIcon from "~/components/icons/regen";
+import PopupIcon from "~/components/icons/popup-icon";
 
 type ProfileType =
   paths["/api/profile"]["get"]["responses"]["200"]["content"]["application/json"];
@@ -81,23 +83,40 @@ const NodesConfig = ({ loaderData }: Route.ComponentProps) => {
               </div>
             </td>
             <td className="w-[25%]">
-              {/* <div className={"bg-card-secondary rounded-lg pl-4 py-2 mr-5"}>
-                Trusted
-              </div> */}
-              <details className="relative inline-block text-left w-full mr-5">
-                <summary className="list-none cursor-pointer flex justify-between items-center gap-x-1.5 bg-card-secondary rounded-lg px-4 py-2 mr-5">
+              <details className="relative inline-block text-left w-full mr-5 group">
+                <summary className="list-none cursor-pointer flex justify-between items-center gap-x-1.5 bg-card-secondary rounded-lg px-4 py-2 mr-5 hover:bg-accent group-open:bg-accent">
                   <div>Options</div>
                   <ChevronIcon direction="down" size={4} />
                 </summary>
 
-                <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5">
+                <div className="absolute left-0 right-5 z-10 mt-2 max-w-full origin-top-right rounded-md bg-card-secondary shadow-lg ">
                   <div className="py-1">
                     <form action="#" method="POST">
                       <button
                         type="submit"
-                        className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        className="block w-full px-4 py-2 text-left text-sm hover:bg-accent"
                       >
-                        Sign out
+                        Trusted
+                      </button>
+                    </form>
+                  </div>
+                  <div className="py-1">
+                    <form action="#" method="POST">
+                      <button
+                        type="submit"
+                        className="block w-full px-4 py-2 text-left text-sm hover:bg-accent"
+                      >
+                        Semi-trusted
+                      </button>
+                    </form>
+                  </div>
+                  <div className="py-1">
+                    <form action="#" method="POST">
+                      <button
+                        type="submit"
+                        className="block w-full px-4 py-2 text-left text-sm hover:bg-accent"
+                      >
+                        Untrusted
                       </button>
                     </form>
                   </div>
@@ -105,8 +124,32 @@ const NodesConfig = ({ loaderData }: Route.ComponentProps) => {
               </details>
             </td>
             <td className="w-[25%]">
-              <div className={"bg-card-secondary rounded-lg pl-4 py-2 mr-5"}>
-                ...kfkfkf
+              <div
+                className={
+                  "bg-card-secondary rounded-lg pl-4 pr-2 py-1 mr-5 flex items-center justify-between"
+                }
+              >
+                <div className="flex items-center gap-x-2">
+                  <p>...kfkfkf</p>
+                  <Popup content={"full certificate"}>
+                    <PopupIcon className="w-5 h-5" />
+                  </Popup>
+                </div>
+                <div className="">
+                  <Popup
+                    content={"Regenerate certificate"}
+                    className="w-32"
+                    bg={"accent"}
+                  >
+                    <Button
+                      variant={"icon"}
+                      size={"icon"}
+                      className="m-0 p-2 w-fit h-fit hover:bg-accent"
+                    >
+                      <RegenerateIcon size={4} />
+                    </Button>
+                  </Popup>
+                </div>
               </div>
             </td>
             <td>
