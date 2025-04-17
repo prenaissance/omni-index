a77ffd15c531a8953489a7bcb28d254a3166c336
alya1007 <alexandra.konjevic@isa.utm.md>
2025-04-17 15:47:25 +0300

    minor(frontend): fix eslint warnings

--- apps/frontends/omni-book/app/routes/admin/node-schema.ts
+++ apps/frontends/omni-book/app/routes/admin/node-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const urlRegex =
-  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 
 export const nodeSchema = z.object({
   url: z.string().regex(urlRegex, {
