8551a214ab03410b915ab74abfa4da187e8932da
alya1007 <alexandra.konjevic@isa.utm.md>
2025-05-02 02:00:28 +0300

    feat(frontend): style book cardon search page

--- apps/frontends/omni-book/app/components/search-books/book-card.tsx
+++ apps/frontends/omni-book/app/components/search-books/book-card.tsx
@@ -8,39 +8,41 @@ export type BookCardProps = {
 
 export const BookCard = ({ book }: BookCardProps) => {
   return (
-    <article className="flex flex-col items-center justify-normal w-54">
-      <Link to={`/books/${book._id}`}>
-        <img
-          src={
-            book.thumbnail && "url" in book.thumbnail
-              ? book.thumbnail.url
-              : "./placeholder.jpg"
-          }
-          alt={book.title}
-          className="h-48 w-32 object-cover"
-        />
-      </Link>
-      <section className="flex flex-col w-full">
-        <Link
-          to={`/books/${book._id}`}
-          className="lg:text-lg font-semibold overflow-hidden [text-wrap:balance] line-clamp-3 hover:text-textHover transition duration-300 text-center"
-        >
-          {book.title}
+    <article className="flex flex-col items-center justify-center bg-card-secondary rounded-md px-4">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <Link to={`/books/${book._id}`}>
+          <img
+            src={
+              book.thumbnail && "url" in book.thumbnail
+                ? book.thumbnail.url
+                : "./placeholder.jpg"
+            }
+            alt={book.title}
+            className="h-48 w-40 object-left-top"
+          />
         </Link>
-        <div className="flex items-center justify-center text-xs lg:text-sm w-full">
-          <div>
-            <StarIcon />
-          </div>
-          <p className="pt-1 mr-2">4.5</p>
-          <div className="w-[1px] h-4 bg-white mr-2"></div>
+        <section className="flex flex-col w-full">
           <Link
-            to={`/search?author=${book.author}`}
-            className="text-md pt-1 hover:text-textHover transition duration-300 text-ellipsis overflow-hidden text-nowrap"
+            to={`/books/${book._id}`}
+            className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap hover:text-textHover transition duration-300 text-base text-center w-44 mx-auto"
           >
-            {book.author}
+            {book.title}
           </Link>
-        </div>
-      </section>
+          <div className="flex items-center justify-center text-xs lg:text-sm w-44 mx-auto">
+            <div>
+              <StarIcon size={4} />
+            </div>
+            <p className="pt-1 mr-2">4.5</p>
+            <div className="w-[1px] h-4 bg-white mr-2"></div>
+            <Link
+              to={`/search?author=${book.author}`}
+              className="text-md pt-1 hover:text-textHover transition duration-300 text-ellipsis overflow-hidden text-nowrap"
+            >
+              {book.author}
+            </Link>
+          </div>
+        </section>
+      </div>
     </article>
   );
 };
--- apps/frontends/omni-book/app/components/search-books/search-books-results.tsx
+++ apps/frontends/omni-book/app/components/search-books/search-books-results.tsx
@@ -7,7 +7,7 @@ export type SearchBooksResultsProps = {
 
 export const SearchBookResults = ({ books }: SearchBooksResultsProps) => {
   return (
-    <div className="flex-1 grid grid-cols-2 md:grid-cols-3 gap-4 lg:grid-cols-5">
+    <div className="flex-1 grid grid-cols-2 md:grid-cols-3 gap-8 lg:grid-cols-5">
       {books.map((book) => (
         <BookCard key={book._id} book={book} />
       ))}
